e2de8e2d13a08f6ff0cca8266b07d91b3190affe
Felipe Matos Santana <felipems@yahoo.com.br>
2017-02-18 04:07:46 -0300

    Add conditional get

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 import json
-from flask import Flask
+from flask import Flask, request, make_response
 from flask_compress import Compress
 from flask_sqlalchemy import SQLAlchemy
-from flask import redirect, request, url_for, Response
 from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE,SIG_DFL)
 from snippets import crossdomain
@@ -17,12 +16,17 @@ db = SQLAlchemy(app)
 
 with open("turmas.json") as json_file:
     json_data = json.load(json_file)
-    jsons = json.dumps(json_data)
+    jsons = json.dumps(json_data, separators=(',', ':'))
 
 @app.route("/", methods=['GET', 'OPTIONS'])
 @crossdomain(origin='*')
 def get_data():
-    return Response(jsons, mimetype='application/json')
+    response = make_response()
+    response.mimetype = 'application/json'
+    response.set_data(jsons)
+    response.add_etag()
+    response.make_conditional(request)
+    return response
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
